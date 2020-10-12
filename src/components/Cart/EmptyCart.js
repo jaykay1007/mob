704fee610c5b23d805dc109ef059424dfbaa0578
jaykay1007 <69098459+jaykay1007@users.noreply.github.com>
2020-10-12 19:33:45 +0530

    first commit

---
+++ src/components/Cart/EmptyCart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function EmptyCart() {
+    return (
+        <div className="container mt-5">
+            <div className="col-10 mx-auto text-center text-title">
+                your cart is currently empty
+                </div>
+        </div>
+    )
+}
