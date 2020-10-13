f46d521045f448737595770e0c893914d637d5ab
jaykay1007 <69098459+jaykay1007@users.noreply.github.com>
2020-10-13 14:56:12 +0530

    Updates

---
+++ precache-manifest.cbcbee39bd5e974a12f9780fe39965ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308e71f7b4caae59b1855f0d75f04709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f845235bba18c2ace64",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "83d057634c10e827d199",
+    "url": "/static/css/main.cf502a38.chunk.css"
+  },
+  {
+    "revision": "7f845235bba18c2ace64",
+    "url": "/static/js/2.adf84aa1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.adf84aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d057634c10e827d199",
+    "url": "/static/js/main.00fa2eeb.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mob/precache-manifest.aa49522d224e7efd0b754a764ab774ae.js"
+  "/precache-manifest.cbcbee39bd5e974a12f9780fe39965ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mob/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
